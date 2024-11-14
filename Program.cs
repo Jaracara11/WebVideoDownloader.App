06dbab60b0c4f8c0e374efbaf5f92742f252bb55
Luis_Eduardo <undernet1@gmail.com>
2024-11-14 13:21:55 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Photino.NET;
+
+namespace WebVideoDownloader.App;
+
+public class Program
+{
+  [STAThread]
+  public static void Main(string[] args)
+  {
+    var window = new PhotinoWindow().SetTitle("WebVideoDownloader.App").Load("wwwroot/index.html");
+
+    window.WaitForClose();
+  }
+}
