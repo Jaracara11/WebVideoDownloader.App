cd604f1514da4e05948207da5c88d49c7f90bf23
Luis_Eduardo <undernet1@gmail.com>
2024-11-20 09:01:19 -0400

    Refactored js script

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -1,35 +1,55 @@
-function sendUrl() {
-  const urlInput = document.getElementById("url-input").value;
+const downloadButton = document.getElementById("download-btn");
+const statusDiv = document.getElementById("status");
+const urlInput = document.getElementById("url-input");
 
-  if (urlInput) {
-    window.external.sendMessage(urlInput);
+const toggleButtonState = (isDisabled) => downloadButton.disabled = isDisabled;
+
+const sendUrl = () => {
+  const url = urlInput.value.trim();
+
+  if (url) {
+    toggleButtonState(true);
+    statusDiv.innerText = "Downloading... Please wait.";
+    window.external.sendMessage(url);
   } else {
-    document.getElementById("status").innerText = "Please provide a valid video URL.";
+    statusDiv.innerText = "Please provide a valid video URL.";
+    toggleButtonState(false);
   }
-}
+};
+
+const handleFileReady = (filePath) => {
+  const downloadMessage = `File successfully downloaded at: ${filePath}`;
+  const pTag = document.createElement('p');
+  const link = document.createElement('a');
+
+  link.href = "#";
+  link.innerText = "Click here to open the folder with the downloaded file";
+  link.onclick = () => {
+    window.external.sendMessage(`OpenFolder:${filePath}`);
+  };
+
+  pTag.innerText = downloadMessage;
+  pTag.style.color = 'green';
+  statusDiv.innerHTML = '';
+  statusDiv.appendChild(pTag);
+  statusDiv.appendChild(link);
+
+  toggleButtonState(false);
+};
+
+const handleError = (errorMessage) => {
+  statusDiv.innerText = errorMessage;
+  statusDiv.style.color = 'red';
+  toggleButtonState(false);
+};
 
 window.external.receiveMessage((message) => {
-  const statusDiv = document.getElementById('status');
-
   if (message.startsWith("FileReady:")) {
     const filePath = message.replace("FileReady:", "");
-    const downloadMessage = `File successfully downloaded at: ${filePath}`;
-
-    const pTag = document.createElement('p');
-    const link = document.createElement('a');
-    link.href = "#";
-    link.innerText = "Click here to open the folder with the downloaded file";
-    link.onclick = () => {
-      window.external.sendMessage(`OpenFolder:${filePath}`);
-    };
-
-    pTag.innerText = downloadMessage;
-    pTag.style.color = 'green';
-    statusDiv.innerHTML = '';
-    statusDiv.appendChild(pTag);
-    statusDiv.appendChild(link);
+    handleFileReady(filePath);
   } else if (message.startsWith("Error:")) {
-    statusDiv.innerText = message.replace("Error:", "");
-    statusDiv.style.color = 'red';
+    handleError(message.replace("Error:", ""));
   }
 });
+
+downloadButton.addEventListener('click', sendUrl);
